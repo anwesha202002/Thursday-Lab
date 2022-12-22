8a503ac8db37a02e9a925b0c241297c43bdbd805
anwesha202002 <121100301+anwesha202002@users.noreply.github.com>
2022-12-22 19:00:16 +0530

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,14 @@
+public class Factorial
+{
+public static void main(String[] args)
+{
+int i,fact=1;
+int a=5;
+for(i=1;i<=5;i++)
+{
+fact=fact*i;
+}
+System.out.println("Factorial of "+5+" is "+fact);
+}
+}
+
